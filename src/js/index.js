0fb4e7375fbd943812219408a9f7856c6667c077
Victor-Pausan <pausanvictor@gmail.com>
2024-04-16 21:43:36 +0300

    first-commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import { HtmlLoader } from "./pageContentLoader.js";
+
+
+// Creating a new instance of the HtmlLoader class.
+let htmlLoader = new HtmlLoader();
+htmlLoader.loadHtml("./components/nav-bar.html", "header");
